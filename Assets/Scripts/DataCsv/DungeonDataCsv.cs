9285de2f42ab6c35df103ffb88df28173db7d9b9
sonnd <sonnd-han185@zitga.com.vn>
2023-10-25 00:05:04 +0700

    Added csv floor

---
+++ Assets/Scripts/DataCsv/DungeonDataCsv.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using Sirenix.OdinInspector;
+
+public class DungeonDataCsv : SerializedScriptableObject
+{
+    public Dictionary<int, FloorConfigData> floorMap = new();
+    public Dictionary<int, EnemyConfigData> enemyMap = new();
+
+    private FloorConfigData[] _floorCsvs;
+    private EnemyConfigData[] _enemyCsvs;
+
+    public void CovertEnemyData()
+    {
+        enemyMap = new();
+        foreach (var item in _enemyCsvs)
+        {
+            enemyMap.Add(item.id, item);
+        }
+    }
+
+    public void ConvertFloorData()
+    {
+        floorMap = new();
+        foreach (var item in _floorCsvs)
+        {
+            floorMap.Add(item.floorId, item);
+        }
+    }
+}
+
+public struct FloorConfigData
+{
+    public int floorId;
+    public int numberOfTrial;
+    public int numberOfCard;
+}
+
+public struct EnemyConfigData
+{
+    public int id;
+    public int attack;
+    public int health;
+    public int defense;
+}
